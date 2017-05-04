2cec41dbb921359cf45419c46e9dab42c6d44310
Jay Hostan <hello@jayhostan.com>
2017-05-04 12:19:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+const products = [
+  { name: 'Head & Shoulders Shampoo', price: 4.99, discount: .6 },
+  { name: 'Twinkies', price: 7.99, discount: .45 },
+  { name: 'Oreos', price: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', price: 13.99, discount: .7 },
+];
+
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 },
+];
+
+function reduce(collection, callback, initialValue) {
+    let result = initialValue;
+
+    collection.forEach((product, index) => {
+            result = callback(result, product, index, collection);
+    });
+
+    return result;
+}
+
+function callback(totalPrice, product) {
+    if (product.discount >= .5) {
+        return totalPrice + product.price;
+    }
+    return totalPrice;
+}
+
+function couponCounter(totalAmount, location) {
+    return totalAmount + location.amount
+}
+
+console.log(reduce(products, callback, 0));
+
+console.log(reduce(couponLocations, couponCounter, 3));
+
+console.log(couponLocations.reduce(couponCounter, 0));
